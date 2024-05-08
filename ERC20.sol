eb8cd06dec6681016acbbe405ae236d958dbc58d
Ibrahim Oladotun <55365663+0xStacker@users.noreply.github.com>
2024-05-08 09:43:27 +0100

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.17;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/structs/EnumerableSet.sol";
 
+/*
+See problem at https://docs.base.org/base-camp/docs/erc-20-token/erc-20-exercise/
+*/
+
 contract WeightedVoting is ERC20{
     uint public maxSupply = 1000000 * 10**18;
     uint internal totalClaimed;
